e86257ec1ea2f79f6d712c0acfe591a710738d9d
Ubuntu <ubuntu@ip-10-244-165-30.ec2.internal>
2013-05-01 21:25:13 +0000

    first

---
+++ download.js
@@ -0,0 +1,61 @@
+// Node.js download counter, with LevelDB and Express.js
+// Make a directory called 'files', put your stuff in there.
+
+var express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    leveldb = require('leveldb'),
+    when = require('when'),
+    db;
+
+app.get('/stats', function(req, res){
+    var stats = {};
+    fs.readdir(__dirname + '/files', function(err, files){
+        when.all(files.map(function(file){
+            var d = when.defer();
+            db.get(file, function(err, val){
+                if (val == null){
+                    stats[file] = 0;
+                }
+                else {
+                    stats[file] = parseInt(val, 10);
+                }
+                return d.resolve();
+            });
+            return d.promise;
+        })).then(function(){
+            res.json(stats);
+        });
+    });
+
+});
+
+app.get('/:filename', function(req, res){
+    var filename = __dirname + '/files/' + req.params.filename;
+    fs.exists(filename, function(exists){
+        if (!exists){
+            return res.send(404);
+        }
+        console.log("downloading " + req.params.filename);
+        db.get(req.params.filename, function(err, val){
+            if (val == null){
+                val = 1;
+            }
+            else {
+                val = parseInt(val, 10);
+                val++;
+            }
+            db.put(req.params.filename, val.toString(), function(err){
+                res.download(filename);
+            });
+        });
+    });
+});
+
+leveldb.open("downloads.db", {
+    create_if_missing: true
+    }, function(err, data){
+        db = data;
+        app.listen(8007);
+        console.log("listening on 8007");
+});
